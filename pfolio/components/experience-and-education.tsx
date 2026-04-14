cc748929c4de3ce47520a38b78f2c334fa8147f1
escursio <abhisaikia675@gmail.com>
2026-04-14 13:15:24 +0530

    experience and education section, basic implementation

--- pfolio/components/experience-and-education.tsx
+++ pfolio/components/experience-and-education.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import {Experience, experiences} from "@/data/experience";
+import { Education, educations } from "@/data/education";
+
 import {useState} from "react";
 
 export default function ExperienceAndEducation () {
@@ -12,9 +15,11 @@ export default function ExperienceAndEducation () {
 
     return(
         <>
+        {/*dont forget to check resonsiveness */}
 
             <div className="w-full mt-[2rem] flex justify-center flex-col">
-                <div className="flex gap-[2vw] justify-center w-full">
+
+                <div className="flex gap-[2vw] justify-center w-full mb-7">
                     <button onClick={() => setshowEducation(false)}
                     className={`xl:px-4 xl:py-1 xl:rounded-lg px-3 py-1 rounded-md
                         focus:outline-none focus:ring-2 focus:ring-[#cdd6f4]
@@ -28,7 +33,54 @@ export default function ExperienceAndEducation () {
                         Education
                     </button>
                 </div>
-                <div className="w-full border-t-2 border-[#cdd6f4] opacity-50 my-5"></div>
+
+                <div className="h-64 overflow-y-auto border-2 rounded-lg p-4 text-[#cdd6f4]">
+                    <div className="space-y-4">
+
+                        {!showEducation && experiences.map((experience: Experience) => (
+                            <div key={experience.id}
+                            className="flex flex-col gap-5">
+                                <div className="flex flex-row gap-4">
+                                    <div className="w-15 h-15 rounded-full overflow-hidden">
+                                        <img className="h-full w-full object-cover"
+                                            src={experience.companyLogo}>                                
+                                        </img>
+                                    </div>
+                                    <div>
+                                        <p>{experience.companyName}</p>
+                                        <p>{experience.role}</p>
+                                        <p>{experience.description}</p>
+                                    </div>
+                                    <p className="ml-auto">{experience.startDate} to {experience.endDate}</p>
+                                </div>
+                            </div>
+                        ))
+                        }
+
+                        {showEducation && educations.map((education: Education) => (
+                            <div key={education.id}
+                            className="flex flex-col gap-5">
+                                <div className="flex flex-row gap-4">
+                                    <div className="w-15 h-15 rounded-full overflow-hidden">
+                                        <img className="h-full w-full object-cover"
+                                            src={education.instituteLogo}>                                
+                                        </img>
+                                    </div>
+                                    <div>
+                                        <p>{education.institureName}</p>
+                                        <p>{education.degree}</p>
+                                        <p>{education.grades}</p>
+                                    </div>
+                                    <p className="ml-auto">{education.startDate} to {education.endDate}</p>
+                                </div>
+                            </div>
+                        ))
+                        }
+
+                    </div>
+                </div>
+
+
             </div>
 
         </>
