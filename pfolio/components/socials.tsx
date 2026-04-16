5732c71577abc9d587c18947f50b9a6eec21f6f3
escursio <abhisaikia675@gmail.com>
2026-04-16 18:57:10 +0530

    socials linked

--- pfolio/components/socials.tsx
+++ pfolio/components/socials.tsx
@@ -3,25 +3,46 @@ import { GitHubIcon, LinkedInIcon, GmailIcon, InstagramIcon, XIcon  } from "@/ic
 export default function Socials(){
     return(
         <>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-2 mt-2">
                 <div className="flex gap-4">
-                    <GitHubIcon className="text-[#c3c9d5] mt-[1rem]
-                    hover:text-[#689bec] transition duration-150 ease-in" />
-                    <LinkedInIcon className="text-[#c3c9d5] mt-[1.1rem]
-                    hover:text-[#689bec] transition duration-150 ease-in" />
-                    <GmailIcon className="text-[#c3c9d5] mt-[1.1rem] 
-                    hover:text-[#689bec] transition duration-150 ease-in" />
-                    <InstagramIcon className="text-[#c3c9d5] mt-[1.1rem] 
-                    hover:text-[#689bec] transition duration-150 ease-in" />
-                    <XIcon className="text-[#c3c9d5] mt-[1.2rem] 
-                    hover:text-[#689bec] transition duration-150 ease-in" />
+
+                    <a href="https://github.com/escursio675" target="_blank" rel="noopener noreferrer"
+                    aria-label="GitHub">
+                        <GitHubIcon className="text-[#c3c9d5] mt-[1rem]
+                        hover:text-[#689bec] transition duration-150 ease-in" />
+                    </a>
+
+                    <a href="https://www.linkedin.com/in/saikia-abhimanyu/" 
+                    target="_blank" rel="noopener noreferrer"
+                    aria-label="LinkedIn">
+                        <LinkedInIcon className="text-[#c3c9d5] mt-[1.1rem]
+                        hover:text-[#689bec] transition duration-150 ease-in" />
+                    </a>
+
+                        <GmailIcon className="text-[#c3c9d5] mt-[1.1rem] 
+                        hover:text-[#689bec] transition duration-150 ease-in" />
+
+                    <a href="https://www.instagram.com/escursio675/" target="_blank" rel="noopener noreferrer"
+                    aria-label="Instagram">
+                        <InstagramIcon className="text-[#c3c9d5] mt-[1.1rem] 
+                        hover:text-[#689bec] transition duration-150 ease-in" />
+                    </a>
+
+                    <a href="https://x.com/yu_workss" target="_blank" rel="noopener noreferrer"
+                    aria-label="Twitter">
+                        <XIcon className="text-[#c3c9d5] mt-[1.2rem] 
+                        hover:text-[#689bec] transition duration-150 ease-in" />
+                    </a>
+
                 </div>
-                <button className="bg-[#cdd6f4] text-black 
-                transition duration-200 ease-in-out hover:bg-[#689bec]
-                xl:rounded-lg rounded-md xl:px-2 xl:py-1 px-2 py-1 xl:mt-2 mt-2
-                focus:outline-none focus:ring-2 focus:ring-[#cdd6f4]">
+
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer"
+                className="bg-[#cdd6f4] text-black 
+                    transition duration-200 ease-in-out hover:bg-[#689bec]
+                    xl:rounded-lg rounded-md xl:px-2 xl:py-1 px-2 py-1 xl:mt-2 mt-2
+                    focus:outline-none focus:ring-2 focus:ring-[#cdd6f4]">
                     Download my Resume!
-                </button>
+                </a>
             </div>
         </>
     )
--- pfolio/data/text.ts
+++ pfolio/data/text.ts
@@ -1,3 +1,3 @@
 export const intro: string = `Currently in my 4th year of Bachelor's in Computer Science and Engineering
- @JEC, Assam. I primarily work frontend although I also work Fullstack with MERN. 
- I also work with Embedded Systems and enjoy low-level programming.`
+ @JEC, Assam. I primarily work in frontend with the MERN stack. I also enjoy low-level programming, 
+ and am on a journey in the very intriguing world of Embedded Systems.`
