8bb18423aaf4897cf9f329b4f354ecfe976c901e
escursio <abhisaikia675@gmail.com>
2026-06-29 20:40:48 +0530

    improve: Glance section

--- pfolio/components/glance.tsx
+++ pfolio/components/glance.tsx
@@ -1,9 +1,55 @@
 
 export default function Glance() {
     return(
-        <div>
+        <div className="mt-2">
             <h1 className="text-[2.5rem]">At a Glance</h1>
-            
+
+            <div className="flex xl:flex-row flex-col xl:gap-[1vw] gap-[3vw] justify-between
+            mt-2 mb-2">
+
+                <div className="border-2 border-[#689bec] rounded-lg w-full
+                xl:h-[30vh] h-[25vh] p-4">
+                    <div>
+                        <h3 className="text-lg font-semibold mb-3">Languages</h3>
+
+                        <ul className="space-y-2">
+                            <li className="flex justify-between items-center">
+                            <span>English</span>
+                            <span className="text-sm text-gray-500">Native</span>
+                            </li>
+
+                            <li className="flex justify-between items-center">
+                            <span>Hindi</span>
+                            <span className="text-sm text-gray-500">Fluent</span>
+                            </li>
+
+                            <li className="flex justify-between items-center">
+                            <span>Assamese</span>
+                            <span className="text-sm text-gray-500">Native</span>
+                            </li>
+
+                            <li className="flex justify-between items-center">
+                            <span>Japanese</span>
+                            <span className="text-sm text-gray-500">Beginner</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+
+                <div className="border-2 border-[#689bec] rounded-lg w-full
+                xl:h-[30vh] h-[25vh]">
+                    Click Me
+                </div>
+
+
+                <div className="border-2 border-[#689bec] rounded-lg w-full
+                xl:h-[30vh] h-[25vh]">
+                    Location
+                </div>
+
+
+            </div>
         </div>
     )
 }
