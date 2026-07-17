c85a89b51129fe924d886aa36d0895b7e2d60a3d
escursio <abhisaikia675@gmail.com>
2026-07-17 22:14:23 +0530

    update: font changes

--- pfolio/app/layout.tsx
+++ pfolio/app/layout.tsx
@@ -1,5 +1,5 @@
-import './globals.css';
-import { Geist } from "next/font/google";
+import "./globals.css";
+import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 
 import type { Metadata } from "next";
@@ -9,8 +9,11 @@ export const metadata: Metadata = {
   description: "Portfolio website for someone who really likes computers",
 };
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
-
+const robotoMono = localFont({
+  src: "../fonts/RobotoMono-Regular.ttf",
+  variable: "--font-roboto-mono",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -18,20 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn(robotoMono.variable)}>
       <head>
         <meta name="apple-mobile-web-app-title" content="MyWebSite" />
       </head>
       <body>
-        
-        <div className="flex flex-col
-        h-screen xl:items-start xl:w-[60%]
-        xl:mt-[10%] xl:ml-[15%]
-        items-start m-[10%]">
+        <div
+          className="flex flex-col h-screen items-start m-[10%] xl:items-start xl:w-[60%] xl:mt-[10%] xl:ml-[15%]">
           {children}
         </div>
-
-        
       </body>
     </html>
   );
